1ea567915023a52b2b27f3ef7e09a47c021b4372
amorimty <lcamendonca@usp.br>
2024-06-12 19:36:50 -0300

    versao completamente funcional porem ainda precisando de muitas melhoras, estou tentando melhorar as variaveis com dicionários ou eventualmente objetos para dinamização

--- main.py
+++ main.py
@@ -6,59 +6,57 @@ session = WolframLanguageSession(
     "C:\\Program Files\\Wolfram Research\\Wolfram Engine\\14.0\\WolframKernel.exe"
 )
 
-variables = ["constEl", "Lb", "Lzero", "massaZinha", "velocidade", "hb"]
+variables = ["x", "y", "z", "m", "a", "b"]
 valuesVar = ["0.9", "1.888", "0.313", "0.12275", "3.15", "1.097"]
 errValues = ["0.5", "0.001", "0.001", "0.00001", "0.11", "0.001"]
 
-expression = (
-    "D[((constEl(Lb-Lzero)^2)/2) + (massaZinha*(velocidade)^2)/2 + massaZinha*9.81*hb, "
-)
 
-sum = ""
+varDict = {
+    "x_": {"value": "", "errValue": ""},
+    "y_": {"value": "", "errValue": ""},
+    "z_": {"value": "", "errValue": ""},
+    "m_": {"value": "", "errValue": ""},
+    "a_": {"value": "", "errValue": ""},
+    "b_": {"value": "", "errValue": ""},
+}
+
+expression = "D[((x(y-z)^2)/2) + (m*(a)^2)/2 + m*9.81*b, "
+
+
+multiVarFunc = "f[x_,y_,z_,m_,a_,b_]:=((x*(y-z)^2)/2)+(m*(a)^2)/2+m*9.81*b"
+
+session.evaluate(wlexpr(multiVarFunc))
+
+sumFunction = ""
 
 for i in range(len(variables)):
 
-    # expressionManipulated = expression.replace("err", errValues[i])
+    diffExp = "D[f[x_,y_,z_,m_,a_,b_],{0}]".format(variables[i] + "_")
 
-    completeExp = expression + "{0}]".format(variables[i])
-
-    resultToString = session.evaluate(wlexpr("ToString[{0}]".format(completeExp)))
-    resultTextString = session.evaluate(wlexpr("TextString[{0}]".format(completeExp)))
+    resultToString = session.evaluate(wlexpr("ToString[{0}]".format(diffExp)))
+    resultTextString = session.evaluate(wlexpr("TextString[{0}]".format(diffExp)))
 
     print(
-        "derivada em relacao a {0} com erro associado {1}:\n {2} \n".format(
+        "derivada em relacao a {0}_ com erro associado {1}:\n {2} \n".format(
             variables[i],
             errValues[i],
             resultToString,
         )
     )
     if variables[i] == variables[0]:
-        sum = sum + "Abs[{0}]*{1}".format(resultTextString, errValues[i])
+        sumFunction = sumFunction + "Abs[{0}]*{1}".format(
+            resultTextString, errValues[i]
+        )
     else:
-        sum = sum + "+ Abs[{0}]*{1}".format(resultTextString, errValues[i])
-    # results.append(resultTextString)
+        sumFunction = sumFunction + "+Abs[{0}]*{1}".format(
+            resultTextString, errValues[i]
+        )
 
 for i in range(len(variables)):
-    sum = sum.replace(variables[i], valuesVar[i])
+    sumFunction = sumFunction.replace(
+        ("Pattern[{0}, Blank[]]".format(variables[i])), valuesVar[i]
+    )
 
-print("\n\n\n resultado da soma: {0}".format(session.evaluate(wlexpr(sum))))
-
-# print(session.evaluate(wlexpr("ToString[{0}]".format(sum))))
-
-
-# expressionParse = session.evaluate(wlexpr("ToString[{0}]".format("(y*x^(2/3))")))
-
-# print("Função a ser derivada parcialmente: \n", expressionParse, "\n")
-
-# result = session.evaluate(wlexpr(expression))
-
-# parse = "TextString[{0}]".format(result)
-# parseX = "ToString[{0}]".format(expressionX)
-# parseY = "ToString[{0}]".format(expressionY)
-
-# print("Resultado sem parsing: \n", result, "\n")
-# print("Resultado com parsing: \n", session.evaluate(wlexpr(parse)), "\n")
-# print("Resultado da derivada parcial em x: \n ", session.evaluate(wlexpr(parseX)), "\n")
-# print("Resultado da derivada parcial em y: \n ", session.evaluate(wlexpr(parseY)), "\n")
+print("\n\n\n resultado da soma: {0}".format(session.evaluate(wlexpr(sumFunction))))
 
 session.terminate()
